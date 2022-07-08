becbd966613d047c4f10c668229b19c2ea40df14
ejaee <ejae8319@gmail.com>
2022-07-08 12:21:59 +0900

    ready to eval

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ejachoi <ejachoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 18:55:14 by ejachoi           #+#    #+#             */
-/*   Updated: 2022/07/08 11:36:14 by ejachoi          ###   ########.fr       */
+/*   Updated: 2022/07/08 12:09:54 by ejachoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*p;
 
-	if ((count == SIZE_MAX && size > 1) || (count > 1 && size == SIZE_MAX))
+	if ((count * size > SIZE_MAX && size > 1) || (count > 1 && size == SIZE_MAX))
 		return (NULL);
 	p = malloc(size * count);
 	if (!p)
