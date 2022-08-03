fb1e47e67fdb48cce295aff2c57729d2f3f293c1
ejaee <ejae8319@gmail.com>
2022-08-03 22:55:04 +0900

    start printf

---
+++ Circle_1/printf/ft_printf.c
@@ -0,0 +1,15 @@
+#include "printf.h"
+
+int ft_printf(const char *format, ...)
+{
+	int	len;
+	va_list ap;
+
+
+	while (*format)
+	{
+		if (*format == '%')	//
+
+		else
+	}
+}
---
+++ Circle_1/printf/print_width_issue.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int	main(){
+	int z = printf("z : %d\n", 42);
+	int t = printf("t : %1d\n", 42);	//	ignored width
+	int a = printf("a : %30d\n", 42);  //	fail
+	int b = printf("b : %2147483648d\n", 42); //	width = 0
+	int c = printf("c : %9223372036854775809d\n", 42); //	width = 0
+	int d = printf("d : %*d\n", 2147483647, 42);
+
+	printf("\n\n---check---\n\n");
+
+	printf("z : %d\n", z);
+	printf("t : %d\n", t);
+	printf("a : %d\n", a);
+	printf("b : %d\n", b);
+	printf("c : %d\n", c);
+	printf("d : %d\n", d);
+
+	printf("\n\n---width check---\n\n");
+
+	int width = 1;
+
+	for (int	i = 1; i < 8; width *= 2, i++)
+		printf("%*d\n", width, i);
+
+	printf("%30d\n", 42);
+	printf("%9223372036854775808d\n", 42);
+	printf("%9223372036854775809d\n", 42);
+}
